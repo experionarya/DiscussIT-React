bca0ab1b27c4a71f3013b67da222f666deed270c
Arya S Vijayan <arya.vijayan@experionglobal.local>
2024-07-23 16:00:40 +0530

    preference list

--- src/features/Announcements/Components/Announcement/index.tsx
+++ src/features/Announcements/Components/Announcement/index.tsx
@@ -1,11 +1,13 @@
 import React, { ReactElement } from "react";
+
 export default function Announcement(): ReactElement {
   return (
     <>
       <article className="w-full space-y-3 overflow-hidden rounded-md bg-white p-3 shadow-sm">
-      <div className="flex justify-between mb-1">
+        <div className="flex justify-between mb-1">
           <h5 className="font-semibold text-slate-900">
-            Hey Experionites!Hey Experionites!Hey Experionites!Hey Experionites!Hey Experionites!
+            Hey Experionites!Hey Experionites!Hey Experionites!Hey
+            Experionites!Hey Experionites!
           </h5>
           <span className="text-xs leading-tight text-slate-500 pt-1 shrink-0">
             October 15, 2024
