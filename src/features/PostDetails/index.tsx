3da97ee6cef5cd46d893512161ce901ba81aee23
vijitha.p <vijithaseven@gmail.com>
2024-09-23 11:22:13 +0530

    post-loading

--- src/features/PostDetails/index.tsx
+++ src/features/PostDetails/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { useLocation, useNavigate } from "react-router-dom";
 
@@ -7,6 +7,7 @@ import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 import { Thread } from "./components/Thread";
 import { Comments } from "./components/Comments";
 import { Replies } from "./components/Replies";
+import { Loading } from "src/components";
 
 import { usePostDetailsStore } from "./store/postDetailsStore";
 import { useAuth } from "src/utils/authenticationHelper/authProvider";
@@ -28,9 +29,22 @@ export default function PostDetails(): ReactElement {
     React.useCallback((state: any) => state.postDetails, [])
   );
 
+  const isPostDetailsLoading = usePostDetailsStore(
+    React.useCallback((state: any) => state.isPostDetailsLoading, [])
+  );
   const threadId = location.search.split("threadId=")[1];
 
   const from = location.state?.from || "/"; // Default to '/' if no state is present
+ 
+  useEffect(() => {
+    if (threadId) {
+      getPostDetailsInfo({
+        token: getParsedToken(),
+        tokenType,
+        threadId: threadId,
+      });
+    }
+  }, [threadId, getPostDetailsInfo, tokenType]);
 
   useQuery(
     ["get_post_details", { threadId: threadId }],
@@ -56,11 +70,11 @@ export default function PostDetails(): ReactElement {
       </div>
       <div className="grid grow grid-cols-3 gap-4 pl-10">
         <div className="col-span-2 space-y-2 pb-7">
-          <article className="w-full space-y-3 overflow-hidden rounded-md bg-white p-3 shadow-sm">
+          {isPostDetailsLoading ? <div className="flex justify-center items-center h-screen"><Loading/></div> : <article className="w-full space-y-3 overflow-hidden rounded-md bg-white p-3 shadow-sm">
             <Thread postDetails={postDetails} setShowComment={setShowComment} />
             <Comments postDetails={postDetails} />
             <Replies postDetails={postDetails} />
-          </article>
+          </article>}
         </div>
         <div className="col-span-1" />
       </div>
