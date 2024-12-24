71a94193768f9e2f7177f37de1f2ddae6bba4392
kailasnadhj <kailas.j@experionglobal.com>
2024-12-24 10:55:36 +0530

    scroll issue resolved in home

--- src/features/Home/components/MiddlePanel/index.tsx
+++ src/features/Home/components/MiddlePanel/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useCallback } from "react";
+import React, { ReactElement, useCallback, useEffect } from "react";
 
 import { PostItem } from "./PostItem";
 import { Loading } from "src/components";
@@ -7,6 +7,7 @@ import { useGetAllPosts } from "../../api";
 
 import { BookMark } from "../../types/bookMarkDataType";
 import { useHomeStore } from "../../store/homeStore";
+import { useLocation } from "react-router-dom";
 
 
 export default function MiddlePanel(): ReactElement {
@@ -19,6 +20,30 @@ export default function MiddlePanel(): ReactElement {
     filterBy: filterByValue,
   });
 
+  const location = useLocation();
+
+  useEffect(() => {
+    // We want to remember scroll position only when we leave Page1
+    const savedScrollPosition = sessionStorage.getItem('page1-scroll');
+
+    if (savedScrollPosition) {
+      window.scrollTo(0, Number(savedScrollPosition));
+    }
+
+    const handleScroll = () => {
+      // Store scroll position in sessionStorage whenever the user scrolls
+      sessionStorage.setItem('page1-scroll', window.scrollY.toString());
+    };
+
+    // Add scroll event listener to track scroll position
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup the event listener on unmount or when the location changes
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [location]);
+  
   return (
     <div className="col-span-2 space-y-3 pb-7">
       {isLoading ? (
